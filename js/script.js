7c32d8a93a0694520df01fa6c49ebd170dad68a7
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-22 14:36:47 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-for(let i=1;i<=100;i++){
 
-    if(i%3==0 && i%5==0 ){
+for(let i=1;i<=100;i++){
+    
+    if(i%3==0 && i%5==0){
         console.log("FizzBuzz");
+        let div =document.createElement("div")
+        div.innerHTML="FizzBuzz";
+        document.getElementById("container").appendChild(div);
     }
     else if(i%3==0){
         console.log("Fizz");
+        let div =document.createElement("div")
+        div.innerHTML="Fizz";
+        document.getElementById("container").appendChild(div);
     }
 
     else if(i%5==0){
         console.log("Buzz");
+        let div = document.createElement("div")
+        div.innerHTML="Buzz";
+        document.getElementById("container").appendChild(div);
     }
 
     else{
         console.log(i);
-    }
+        let div =document.createElement("div")
+        div.innerHTML=i;
+        document.getElementById("container").appendChild(div);
+    } 
 }
